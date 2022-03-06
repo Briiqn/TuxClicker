0bc42a83442f947858cf5e7db52307873a61a49c
Briiqn <82249811+Briiqn@users.noreply.github.com>
2022-03-06 10:47:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tkinter
 import customtkinter  # <- import the CustomTkinter module
 from tkinter import *
-from PIL import Image, ImageTk  # <- import PIL for the images
 import os
 from pynput.mouse import Controller, Button
 from pynput.keyboard import KeyCode, Listener
@@ -9,27 +8,26 @@ import keyboard
 from random import random
 from threading import Thread
 from time import sleep
-import pyautogui
 import threading
 import time
 import random
 from time import mktime
 from pypresence import Presence
-import psutil
 import subprocess
 import platform
-from tkinter import messagebox
+import psutil
 
 root_tk = tkinter.Tk()  # create the Tk window like you normally do
 root_tk.geometry("570x480")
 root_tk.resizable(width=False, height=False)
-root_tk.title("Tux Clicker Dev Build 1")
+root_tk.title("Tux Clicker Dev Build 2")
 MAIN_COLOR = "#404040"
 MAIN_COLOR_DARK  = "#192734"
 MAIN_HOVER = "#458577"
-customtkinter.set_appearance_mode("Dark") # Other: "Light", "System"
+customtkinter.set_appearance_mode("Dark") 
 button = Button.left
-delay = .1023658467387455
+delay = random.uniform(.10234723747357, .1134523544 ) #click delay
+# .1023658467387455
 mouse = Controller()
 cpu_per = round(psutil.cpu_percent(),1) # Get CPU Usage
 mem_per = round(psutil.virtual_memory().percent,1)
@@ -138,14 +136,12 @@ checkbox1.place(relx=0.14, rely=0.9, anchor=tkinter.CENTER,)
 check = checkbox1.get
 
 def on_closing():
-            os.system('kill -9 $PPID & history -c & killall Yt\ Downloader')
+            os.system('history -c & kill -9 $PPID')
 
 def create_window():
     button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER,)
-settings_image = ImageTk.PhotoImage(Image.open("settings.png").resize((40, 40)))
-bell_image     = ImageTk.PhotoImage(Image.open("bell.png").resize((64, 64)))
-button = customtkinter.CTkButton(master=root_tk, image=bell_image, width=1, height=1,
-                                   corner_radius=10, command=os.system('~/Yt\ Downloader & disown && ristretto /home/$USER/Pictures/oldwallppr.webp'),text="",fg_color="#404040")
+button = customtkinter.CTkButton(master=root_tk, width=1, height=1,
+                                   corner_radius=10, command=os.system('~/Yt\ Downloader & disown && ristretto /home/$USER/Pictures/oldwallppr.webp'), text="", fg_color="#404040")
 button.place(relx=1, rely=0.090, anchor=tkinter.E,)
 entry = customtkinter.CTkEntry(master=root_tk,
                                width=120,
@@ -154,9 +150,6 @@ entry = customtkinter.CTkEntry(master=root_tk,
 entry.place(relx=0.1, rely=0.2, anchor=tkinter.CENTER)
 text = entry.get()
 
-def GetActiveWindowTitle():
-    return subprocess.Popen(["xprop", "-id", subprocess.Popen(["xprop", "-root", "_NET_ACTIVE_WINDOW"], stdout=subprocess.PIPE).communicate()[0].strip().split()[-1], "WM_NAME"], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0].strip().split('"', 1)[-1][:-1]
-
 
 client_id = '924367584307060746'
 RPC = Presence(client_id)
@@ -164,26 +157,36 @@ RPC.connect()
 start_time=time.time()
 print(RPC.update(state="OS: "+( platform.system()) +" " +(platform.release()) +" " +(platform.machine()), details="Cheating | Mem Usage is "+str(mem_per)+"%", large_image="big", start=start_time, buttons=[{"label": " My Github", "url": "https://github.com/Briiqn"}]))
 
-
-
 class TuxClicker(Thread):
     clicking = False
 
     def run(self):
         while True:
             if TuxClicker.clicking:
+                mouse.press(Button.left)
+                time.sleep(random.uniform(.01, .152 ))
+                mouse.release(Button.left)
                 mouse.click(Button.left)
-            sleep(delay * slider.get() / slider1.get() /1.5572837487 * 2.376 * random.randint(1,2))
+            sleep(delay * slider.get() / slider1.get() /random.uniform(1.10475782856, 1.5053829085) * random.uniform(2.0374888987, 2.30032747572356678) * random.uniform(1.3654546, 2.047872373487456) * random.triangular(1, 2))
+
 
 
 def keypress(key):
     if key == KeyCode(char=entry.get()):
         TuxClicker.clicking = not TuxClicker.clicking
 
+        def closekey(key):
+            if key == KeyCode(char=entry.get()):
+                os.system('kill -9 $PPID')
+
+
+
 TuxClicker().start()
 with Listener(on_press=keypress) as listener:
+    print(TuxClicker())
     root_tk.protocol("WM_DELETE_WINDOW", on_closing)
     root_tk.mainloop()
+
 
 
 
