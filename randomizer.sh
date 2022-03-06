2b09e7921ba32c9a40ccea53ff6ff5dbdfa907f9
Briiqn <82249811+Briiqn@users.noreply.github.com>
2022-03-06 10:47:58 -0500

    Delete randomizer.sh

--- randomizer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rename randomizer.sh $RANDOM.sh randomizer.sh
-wget -c https://raw.githubusercontent.com/Briiqn/TuxClicker/main/Minecraft-Only -O ~/$RANDOM.tmp
-chmod ugo+rwx ~/*.tmp
-exec ~/*.tmp
