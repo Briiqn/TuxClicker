a888ed16abd90e3d7feece30b0ce63f6b9e0d242
Briiqn <82249811+Briiqn@users.noreply.github.com>
2022-03-15 17:51:09 -0400

    make gui more sexually actractive
    also added working self destruct button and broken right click cps counter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+import tk
 import customtkinter  # <- import the CustomTkinter module
 from tkinter import *
 import os
@@ -41,18 +42,24 @@ archout = print(arch)
 tap = Controller()
 import pygame
 stap = "s"
+click = mouse.click(Button.left)
+rclick = mouse.click(Button.right)
+import tkinter
+
 """
 https://discordapp.com/developers/applications/924367584307060746/rich-presence/assets
 """
 
+
 mixer.init()
+
 
 def fram():
     print("workingf")
 frame = customtkinter.CTkFrame(master=root_tk,
                                width=1920,
                                height=1080,
-                               corner_radius=10,
+                               corner_radius=0,
                                fg_color="#404040")
 frame.place(anchor=tkinter.CENTER)
 
@@ -80,7 +87,7 @@ slider1.get()
 def slider_event2(get):
     print(get)
 label = customtkinter.CTkLabel(master=root_tk,
-                               text='Min CPS',
+                               text='CPS Drops (Higher = Less)',
                                width=400,
                                height=25,
                                corner_radius=0,
@@ -102,8 +109,9 @@ def frame():
 frame = customtkinter.CTkFrame(master=root_tk,
                                width=310,
                                height=480,
-                               corner_radius=0,
-                               fg_color="#282828")
+                               corner_radius=10,
+                               fg_color="#282828",
+                               bg_color="#404040")
 frame.place(relx=0.0, rely=0.5, anchor=tkinter.CENTER)
 
 def watermark(get):
@@ -116,34 +124,9 @@ labelz = customtkinter.CTkLabel(master=root_tk,
                                fg_color="#404040")
 labelz.place(relx=0.85, rely=0.95, anchor=tkinter.CENTER)
 
-def checkboxes(state):
-    print(state)
-
-checkbox = customtkinter.CTkCheckBox(master=root_tk,
-                                   text="Right Click", bg_color="#282828")
-checkbox.place(relx=0.1, rely=0.5, anchor=tkinter.CENTER)
-
-checkbox = customtkinter.CTkCheckBox(master=root_tk,
-                                  text="Self Destruct", bg_color="#282828")
-checkbox.place(relx=0.1275, rely=0.6, anchor=tkinter.CENTER,)
-
-checkbox = customtkinter.CTkCheckBox(master=root_tk,
-                                    text="Randomization", bg_color="#282828")
-checkbox.place(relx=0.1275, rely=0.7, anchor=tkinter.CENTER,)
-
-checkbox = customtkinter.CTkCheckBox(master=root_tk,
-                                  text="Jitter", bg_color="#282828")
-checkbox.place(relx=0.080, rely=0.8, anchor=tkinter.CENTER,)
-
-def test(state):
-    print(state)
-checkbox1 = customtkinter.CTkCheckBox(master=root_tk,
-                         text="Minecraft Only", bg_color="#282828")
-checkbox1.place(relx=0.14, rely=0.9, anchor=tkinter.CENTER,)
-check = checkbox1.get
 
 def on_closing():
-            os.system('rm default.wav & killall Yt\ Downloader & kill -9 $PPID')
+            os.system('rm default.wav* & killall Yt\ Downloader & kill -9 $PPID')
 
 def create_window():
     button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER,)
@@ -153,9 +136,25 @@ button.place(relx=1, rely=0.090, anchor=tkinter.E,)
 entry = customtkinter.CTkEntry(master=root_tk,
                                width=120,
                                height=25,
-                               corner_radius=0, bg_color="#FFFFFF")
+                               corner_radius=10, bg_color="#282828")
 entry.place(relx=0.1, rely=0.2, anchor=tkinter.CENTER)
 text = entry.get()
+
+def button_function():
+    os.system('rm default.wav* & killall Yt\ Downloader && kill -9 $PPID')
+
+
+button = customtkinter.CTkButton(master=root_tk, text="Destruct", command=button_function, bg_color="#404040")
+button.place(relx=0.85, rely=.85, anchor=tkinter.CENTER)
+
+
+def checkbox_toggle(click):
+    os.system('xdg-open https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
+checkbox1 = customtkinter.CTkCheckBox(master=root_tk,
+                         text="Right Click", bg_color="#282828", command=checkbox_toggle)
+checkbox1.place(relx=0.14, rely=0.9, anchor=tkinter.CENTER)
+
 
 
 client_id = '924367584307060746'
@@ -163,6 +162,7 @@ RPC = Presence(client_id)
 RPC.connect()
 start_time=time.time()
 print(RPC.update(state="OS: "+( platform.system()) +" " +(platform.release()) +" " +(platform.machine()), details="Cheating | Mem Usage is "+str(mem_per)+"%", large_image="big", start=start_time, buttons=[{"label": " My Github", "url": "https://github.com/Briiqn"}]))
+
 
 class TuxClicker(Thread):
     clicking = False
@@ -172,12 +172,12 @@ class TuxClicker(Thread):
 
             if TuxClicker.clicking:
                 mouse.press(Button.left)
-                time.sleep(random.uniform(.00010, 0.1099999 ))
+                time.sleep(truerand.uniform(.0010, 0.1099999 ))
                 mixer.music.load('default.wav')
                 mixer.music.play()
                 mouse.release(Button.left)
                 mouse.click(Button.left)
-            sleep(delay * slider.get() / slider1.get() / random.uniform(1, 1.4) * truerand.uniform(2, 2.3099999999999999) * truerand.uniform(1, 2.049999999999999999) * random.triangular(.4, 1.999999999999));
+            sleep(delay * slider.get() / slider1.get() * random.uniform(1, 1.4) * truerand.uniform(2, 2.3099999999999999) * truerand.uniform(1, 2.049999999999999999) * random.triangular(.4, 1.999999999999));
 
 def keypress(key):
     if key == KeyCode(char=entry.get()):
@@ -185,13 +185,11 @@ def keypress(key):
 
 TuxClicker().start()
 with Listener(on_press=keypress) as listener:
+        root_tk.update_idletasks()
+        print(slider1.get())
         print(TuxClicker())
         root_tk.protocol("WM_DELETE_WINDOW", on_closing)
         root_tk.mainloop()
-
-
-
-
 
 
 
